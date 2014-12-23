2cf0157b036086fdf7efbd2a6e4217d26e9e881f
Jake Weinstein <xboxlover360@gmail.com>
2014-12-22 18:29:03 -0600

    ext4_utils: disable secure discard globally
    * Makes flashing much much faster, no point in an opt-out flag
    
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- ext4_utils/wipe.c
+++ ext4_utils/wipe.c
@@ -42,13 +42,6 @@ int wipe_block_device(int fd, s64 len)
 		// Wiping only makes sense on a block device.
 		return 0;
 	}
-
-#ifndef NO_SECURE_DISCARD
-	range[0] = 0;
-	range[1] = len;
-	ret = ioctl(fd, BLKSECDISCARD, &range);
-	if (ret < 0) {
-#endif /* NO_SECURE_DISCARD */
 		range[0] = 0;
 		range[1] = len;
 		ret = ioctl(fd, BLKDISCARD, &range);
@@ -59,9 +52,6 @@ int wipe_block_device(int fd, s64 len)
 			warn("Wipe via secure discard failed, used discard instead\n");
 			return 0;
 		}
-#ifndef NO_SECURE_DISCARD
-	}
-#endif /* NO_SECURE_DISCARD */
 	return 0;
 #else
 	warn("Wipe via secure discard suppressed due to bug in EMMC firmware\n");
